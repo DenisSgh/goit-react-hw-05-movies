f35c63b6603a39f859ee1c1085ac9268f5844e14
Denis Singh <denis.darkrgd@gmail.com>
2021-08-05 22:18:38 +0300

    Added routers

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-export default function App() {}
+import Container from 'components/Container';
+import Navigation from 'components/Navigation';
+import HomePage from 'views/HomePage';
+import MoviesPage from 'views/MoviesPage';
+import MovieDetailsPage from 'views/MovieDetailsPage';
+
+export default function App() {
+  return (
+    <Container>
+      <Navigation />
+
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/movies" component={MoviesPage}></Route>
+        <Route path="/movies/:movieId" component={MovieDetailsPage}></Route>
+      </Switch>
+    </Container>
+  );
+}
