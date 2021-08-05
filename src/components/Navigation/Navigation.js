4a7078bc8702f80d200fb331c3612d2d4fe8c0d9
Denis Singh <denis.darkrgd@gmail.com>
2021-08-05 22:19:24 +0300

    Added navigation

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,19 @@
+import { NavLink } from 'react-router-dom';
+// import PropTypes from 'prop-types';
+
+import s from './Navigation.module.css';
+
+export default function Navigation() {
+  return (
+    <nav>
+      <NavLink exact to="/" className={s.link} activeClassName={s.activeLink}>
+        Home
+      </NavLink>
+      <NavLink to="/movies" className={s.link} activeClassName={s.activeLink}>
+        Movies
+      </NavLink>
+    </nav>
+  );
+}
+
+// Navigation.propTypes = { onSubmit: PropTypes.func.isRequired };
